18c7fc842b3ff2808164f52b666972000b5123e2
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-04-19 17:49:50 -0700

    working on Tour model

---
+++ tests/models/Tour.test.js
@@ -0,0 +1,28 @@
+const Tour = require('../../lib/models/Tour');
+const mongoose = require('mongoose');
+
+describe('Tour Model', () => {
+  it('has title, activities, launchDate, and stops fields', () => {
+    const date = new Date();
+    const tour = new Tour({
+      title: 'greatest show on earth',
+      activities: ['games', 'fun stuff'],
+      launchDate: date,
+      stops: [{ 
+        location: '36.974018,-122.030952', weather: { 
+          weatherState: 'rainy', 
+          temp: 26 
+        }, 
+        attendance: 145 
+      }]
+    });
+    console.log(tour);
+    expect(tour.toJSON()).toEqual({
+      title: 'greatest show on earth',
+      activities: ['games', 'fun stuff'],
+      launchDate: date.toISOString(),
+      stops: [{ _id: expect.any(mongoose.Types.ObjectId), location: '36.974018,-122.030952', weather: { weatherState: 'rainy', temp: 26 }, attendance: 145 }],
+      _id: expect.any(mongoose.Types.ObjectId)
+    });
+  });
+});
