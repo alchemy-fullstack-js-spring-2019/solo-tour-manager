2a63c166ba4d631dd59df528b951e8fdd61a21ff
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-04-20 14:21:33 -0700

    server is up and listening

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = require('./lib/app');
 const PORT = process.env.PORT || 7890;
 
 app.listen(PORT, () => {
-  console.log(`Server listening in on port: ${PORT}`);
+  console.log(`Server listening in on port:${PORT}`);
 });
