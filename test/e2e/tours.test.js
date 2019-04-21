60a4814c2b4f6fb1d9a2ce28a943b0e64a896deb
Chris Piccaro <chrispiccaro18@gmail.com>
2019-04-21 14:00:34 -0700

    passing post for stops, weird dates tho

--- test/e2e/tours.test.js
+++ test/e2e/tours.test.js
@@ -3,6 +3,9 @@ const request = require('supertest');
 const mongoose = require('mongoose');
 const app = require('../../lib/app');
 const Tour = require('../../lib/models/Tour');
+// const Stop = require('../../lib/models/Stop');
+
+jest.mock('../../lib/services/metaweather-api.js');
 
 describe('tours routes', () => {
   const testLaunchDate = new Date();
@@ -14,6 +17,20 @@ describe('tours routes', () => {
     activities: ['listening', 'swaying', 'Bob Dylan'],
     launchDate: testLaunchDate,
     stops: [stop1, stop2]
+  };
+
+  const testTour2 = {
+    title: 'Never Ending Tour',
+    activities: ['listening', 'swaying', 'Bob Dylan'],
+    launchDate: testLaunchDate
+  };
+
+  const testStop = {
+    location: {
+      lat: 37.77,
+      lon: -122.41
+    },
+    attendance: 1
   };
 
   it('can create a tour', () => {
@@ -71,4 +88,67 @@ describe('tours routes', () => {
         });
       });
   });
+
+  it('can add a stop', () => {
+    return Tour.create(testTour2)
+      .then(createdTour => {
+        return request(app)
+          .post(`/tours/${createdTour._id}/stops`)
+          .send(testStop);
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          __v: 0,
+          location: {
+            lat: 37.77,
+            lon: -122.41,
+            woeid: 2487956,
+            name: 'San Francisco'
+          },
+          weather: {
+            sunRise: '2019-04-21T13:26:32.195Z',
+            sunSet: '2019-04-22T02:49:55.671Z',
+            weatherState: 'Light Cloud',
+            temp: 14.195,
+            humidity: 72
+          },
+          attendance: 1
+        });
+      });
+  });
 });
+
+// "sunRise": "2019-04-21T13:26:32.195Z",
+// "sunSet": "2019-04-22T02:49:55.671Z",
+
+// sun_rise: '2019-04-21T06:26:32.195043-07:00',
+// sun_set: '2019-04-21T19:49:55.671339-07:00',
+
+// {
+//   _id: expect.any(String),
+//   __v: 0,
+//   title: 'Never Ending Tour',
+//   activities: ['listening', 'swaying', 'Bob Dylan'],
+//   launchDate: testLaunchDate,
+//   stops: [
+// {
+//   _id: expect.any(String),
+//   __v: 0,
+//   location: {
+//     lat: 37.77,
+//     lon: -122.41,
+//     woeid: 2487956,
+//     name: 'San Francisco'
+//   },
+//   weather: {
+//     sunRise: '2019-04-21T06:26:32.195043-07:00',
+//     sunSet: '2019-04-21T19:49:55.671339-07:00',
+//     weatherState: 'Light Cloud',
+//     temp: 14.195,
+//     humidity: 72
+//   },
+//   attendance: 1
+// }
+//   ]
+// }
