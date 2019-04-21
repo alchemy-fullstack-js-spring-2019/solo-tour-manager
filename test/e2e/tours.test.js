c91c609012a4b7e44a59fa6ff834c6ba845fb9e9
Chris Piccaro <chrispiccaro18@gmail.com>
2019-04-20 20:49:05 -0700

    get route for tours passing

--- test/e2e/tours.test.js
+++ test/e2e/tours.test.js
@@ -1,26 +1,46 @@
 require('../connect-db');
-
 const request = require('supertest');
 const mongoose = require('mongoose');
 const app = require('../../lib/app');
+const Tour = require('../../lib/models/Tour');
 
 describe('tours routes', () => {
-  it('can create a tour', () => {
-    const testLaunchDate = new Date();
-    const stop1 = new mongoose.Types.ObjectId;
-    const stop2 = new mongoose.Types.ObjectId;
+  const testLaunchDate = new Date();
+  const stop1 = new mongoose.Types.ObjectId;
+  const stop2 = new mongoose.Types.ObjectId;
 
-    const testTour = {
-      title: 'Never Ending Tour',
-      activities: ['listening', 'swaying', 'Bob Dylan'],
-      launchDate: testLaunchDate,
-      stops: [stop1, stop2]
-    };
+  const testTour = {
+    title: 'Never Ending Tour',
+    activities: ['listening', 'swaying', 'Bob Dylan'],
+    launchDate: testLaunchDate,
+    stops: [stop1, stop2]
+  };
+
+  it('can create a tour', () => {
     return request(app)
       .post('/tours')
       .send(testTour)
       .then(res => {
         expect(res.body).toEqual({
+          _id: expect.any(String),
+          title: 'Never Ending Tour',
+          activities: ['listening', 'swaying', 'Bob Dylan'],
+          launchDate: testLaunchDate.toISOString(),
+          stops: [stop1.toString(), stop2.toString()],
+          __v: 0
+        });
+      });
+  });
+
+  it('can get all tours', () => {
+    return Tour.create(testTour)
+      .then(() => {
+        return request(app)
+          .get('/tours');
+      })
+      .then(res => {
+        expect(res.body).toHaveLength(1);
+        expect(res.body[0]).toEqual({
           _id: expect.any(String),
           title: 'Never Ending Tour',
           activities: ['listening', 'swaying', 'Bob Dylan'],
