ec2b0ea4ec4f30d0e9f93d143bb1930a6491ca2d
Chris Piccaro <chrispiccaro18@gmail.com>
2019-04-20 18:21:54 -0700

    Writing first test for tour routes

---
+++ test/e2e/tours.test.js
@@ -0,0 +1,31 @@
+const request = require('supertest');
+const mongoose = require('mongoose');
+const app = require('../../lib/app');
+
+describe('tours routes', () => {
+  it('can create a tour', () => {
+    const testLaunchDate = new Date();
+    const stop1 = new mongoose.Types.ObjectId;
+    const stop2 = new mongoose.Types.ObjectId;
+
+    const testTour = {
+      title: 'Neverending Tour',
+      activities: ['listening', 'swaying', 'Bob Dylan'],
+      launchDate: testLaunchDate,
+      stops: [stop1, stop2]
+    };
+    return request(app)
+      .post('/tours')
+      .send(testTour)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          title: 'Neverending Tour',
+          activities: ['listening', 'swaying', 'Bob Dylan'],
+          launchDate: testLaunchDate,
+          stops: [stop1.toString(), stop2.toString()],
+          __v: 0
+        });
+      });
+  });
+});
