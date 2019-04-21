4b24aa04e5f9adc511bce010fdc12773f512f7bd
ollinieminen <otn987@gmail.com>
2019-04-21 11:13:22 -0700

    can find all stops works

--- test/lib/routes/stops.test.js
+++ test/lib/routes/stops.test.js
@@ -24,7 +24,40 @@ describe('stops route', () => {
                     });
             })
             .then(newStop => {
-                expect(newStop.body).toEqual('7');
+                expect(newStop.body).toEqual({
+                    __v:0,
+                    _id:expect.any(String),
+                    attendance:600,
+                    location:{ cordinates: expect.any(String) },
+                    tour: expect.any(String),
+                    weather:{ weather_state_abbr: expect.any(String) }
+
+                });
+            });
+    });
+    it.only('can find all stops', ()=>{
+        const date = new Date;
+        return Tour
+            .create({
+                title:'first tour',
+                activities: ['poledancing', 'trapese'],
+                date:date
+            })
+            .then(newTour => {
+                const id = newTour._id;
+                return request (app)
+                    .post('/api/v1/stops')
+                    .send({
+                        tour: id,
+                        attendance:600
+                    });
+            })
+            .then(() => {
+                return request(app)
+                    .get('/api/v1/stops')
+                    .then(foundStops => {
+                        expect(foundStops.body.length).toEqual(2);
+                    });
             });
     });
 })
