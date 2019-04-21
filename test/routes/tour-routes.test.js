e1da7ec4dbfdd631084088782d23e2f0107766c5
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-04-20 17:33:37 -0700

    tour find by id passing

--- test/routes/tour-routes.test.js
+++ test/routes/tour-routes.test.js
@@ -15,16 +15,17 @@ describe('Tour routes', () => {
   });
 
   
-  // beforeEach(() => {
-  //   return mongoose.connection.dropDatabase();
-  // });
-  // afterAll(() => {
-  //   return mongoose.connection.close();
-  // });
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
   
   // beforeAll(() => {
   //   return connect();
   // });
+
   it('creates a tour', () => {
     return request(app)
       .post('/tours')
@@ -75,9 +76,56 @@ describe('Tour routes', () => {
     return request(app)
       .get('/tours')
       .then(res => {
-        expect(res.body).toHaveLength(1);
+        expect(res.body).toHaveLength(0);
         expect(res.body).toEqual(expect.any(Array));
       });
   });
 
+  it('finds and returns a tour by id', () => {
+    return request(app)
+      .post('/tours')
+      .send({
+        title: 'Best Little Whore-House in PDX',
+        activities: ['fire-breathing', 'feats of strength', 'juggling'],
+        launchdate: new Date(),
+        stops: [
+          {
+            location: 'Portland',
+            weather: 'Sunny',
+            attendance: 1
+          },
+          {
+            location: 'Seattle',
+            weather: 'Rainy',
+            attendance: 5
+          }
+        ]
+      })
+      .then(createdTour => {
+        return request(app)
+          .get(`/tours/${createdTour.body._id}`);
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          title: 'Best Little Whore-House in PDX',
+          activities: ['fire-breathing', 'feats of strength', 'juggling'],
+          launchdate: expect.any(String),
+          stops: [
+            {
+              location: 'Portland',
+              weather: 'Sunny',
+              attendance: 1,
+              _id: expect.any(String)
+            },
+            {
+              location: 'Seattle',
+              weather: 'Rainy',
+              attendance: 5,
+              _id: expect.any(String)
+            }
+          ],
+          _id: expect.any(String)
+        });
+      });
+  });
 });
