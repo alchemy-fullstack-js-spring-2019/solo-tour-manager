0a73103879b21b803267e05b45f5308ae5e57c00
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-04-21 22:05:34 -0700

    wrote function for second search URL, test pass

--- tests/unit/make-search-url.test.js
+++ tests/unit/make-search-url.test.js
@@ -1,11 +1,18 @@
-const makeUrl = require('../../lib/middleware/make-search-url');
+const { makeLLUrl, makeWoeidUrl } = require('../../lib/middleware/make-search-url');
 
 describe('it makes search url for api', () => {
   it('includes lattlong in search URL', () => {
     const location = '33.845470' + ',130.848557';
 
-    const result = makeUrl(location);
+    const result = makeLLUrl(location);
 
     expect(result).toEqual('https://metaweather.com/api/location/search/?lattlong=33.845470%2C130.848557');
+  });
+  it('makes another URL for woeid search', () => {
+    const woeid = 44418;
+
+    const result = makeWoeidUrl(woeid);
+    
+    expect(result).toEqual('https://metaweather.com/api/location/44418');
   });
 });
