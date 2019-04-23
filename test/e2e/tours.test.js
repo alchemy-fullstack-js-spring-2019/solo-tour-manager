e907504558b9625341a63110fbf491b0fa65131f
Ben Parisot <me@benparisot.com>
2019-04-23 13:10:54 -0700

    fix error that was preventing connection to mongodb

--- test/e2e/tours.test.js
+++ test/e2e/tours.test.js
@@ -1,8 +1,12 @@
+require('dotenv').config();
 require('../connect-to-db');
+// const mongoose = require('mongoose');
+// const connect = require('../../lib/utils/connect');
 const request = require('supertest');
 const app = require('../../lib/app');
 
 describe('tour routes tests', () => {
+
   it('creates a tour', () => {
     return request(app)
       .post('/tours')
