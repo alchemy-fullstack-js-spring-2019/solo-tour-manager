a61c8a94dbab33e9595fc5cd1a483c8ef9e203fa
Ben Parisot <me@benparisot.com>
2019-04-23 21:55:09 -0700

    pass test for /tours route

--- test/e2e/tours.test.js
+++ test/e2e/tours.test.js
@@ -1,26 +1,27 @@
 require('dotenv').config();
 require('../connect-to-db');
 // const mongoose = require('mongoose');
-// const connect = require('../../lib/utils/connect');
 const request = require('supertest');
 const app = require('../../lib/app');
 
 describe('tour routes tests', () => {
-
+  const launchDate = Date.now();
   it('creates a tour', () => {
     return request(app)
       .post('/tours')
       .send({
         title: 'Magic Mystery Tour',
         activities: ['dancing', 'singing', 'juggling'],
-        launchDate: Date.now(),
+        launchDate
       })
       .then(results => {
         expect(results.body).toEqual({
+          __v: 0,
+          _id: expect.any(String),
           title: 'Magic Mystery Tour',
           activities: ['dancing', 'singing', 'juggling'],
-          launchDate: Date.now(),
-          stops: [] 
+          launchDate: expect.any(String),
+          stops: []
         });
       });
   });
