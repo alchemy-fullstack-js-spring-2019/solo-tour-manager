98c11c2ad3795e3d5e25517714213d4bd33cd050
Cara Ponzini <ponzini@gmail.com>
2019-04-23 17:54:30 -0700

    created test for stop schema

---
+++ test/Models/Stop.test.js
@@ -0,0 +1,32 @@
+const Stop = require('../../lib/Models/Stop');
+// const mongoose = require('mongoose');
+
+describe('stop schema test', () => {
+  it('has location, weather, and attendance', () => {
+    const stop = new Stop({
+      location: {
+        lat: 12345,
+        lon: 4444,
+      },
+      weather: {
+        temp_max: 60,
+        temp_min: 40,
+        description: 'cloudy'
+      },
+      attendance: 2
+    });
+
+    expect(stop.toJSON()).toEqual({
+      location: {
+        lat: 12345,
+        lon: 4444,
+      },
+      weather: {
+        temp_max: 60,
+        temp_min: 40,
+        description: 'cloudy'
+      },
+      attendance: 2
+    });
+  });
+});
