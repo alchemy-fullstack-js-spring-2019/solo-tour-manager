9463f484fab6a99e02d8d996b5ffdc14cfd34d77
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-04-25 14:32:30 -0700

    trying to get api call middleware to pass

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
 require('dotenv').config();
-const mongoose = require('mongoose');
+require('./lib/utils/connect')();
 const app = require('./lib/app');
 
 const PORT = process.env.port || 7890;
-
-mongoose.connect(process.env.MONGODB_URI, {
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useCreateIndex: true
-});
 
 app.listen(PORT, () => {
   console.log(`started on port ${PORT}`);
--- tests/routes/tours.test.js
+++ tests/routes/tours.test.js
@@ -1,21 +1,48 @@
 require('dotenv').config();
 const request = require('supertest');
+const mongoose = require('mongoose');
+const connect = require('../../lib/utils/connect');
 const app = require('../../lib/app');
 const Tour = require('../../lib/models/Tour');
 
 describe('tour routes', () => {
+  beforeAll(() => {
+    return mongoose.connect('mongodb://localhost:27017/circus-tour', {
+      useFindAndModify: false,
+      useNewUrlParser: true,
+      useCreateIndex: true
+    });
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  
   it('can create a tour', () => {
     const date = new Date;
     return request(app)
       .post('/api/v1/tours')
-      .send({ title: 'greatest show', activities: ['games'], launchDate: date, stops: [{ 
-        location: '36.974018,-122.030952', 
-        attendance: 100
-      }]
+      .send({
+        title: 'greatest show', 
+        activities: ['games'], 
+        launchDate: date, 
+        stops: [{ 
+          location: '36.974018,-122.030952',
+          weather: {
+            temp: null,
+            weatherState: null
+          },
+          attendance: 100
+        }]
       })
-      .then(createdTour => {
-      
-        expect(createdTour.body).toEqual({
+      .then(result   => {
+        console.log(result.body);
+        expect(result.body).toEqual({
           title: 'greatest show',
           activities: ['games'],
           launchDate: date.toISOString(),
@@ -29,6 +56,7 @@ describe('tour routes', () => {
             attendance: 100
           }],
           _id: expect.any(String),
+          __v: 0
         });
       });
   });
