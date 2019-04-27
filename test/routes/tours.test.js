3e522365cca3c9b74a8e4a396ed9739fb1801659
Cara Ponzini <ponzini@gmail.com>
2019-04-27 15:47:55 -0700

    moved stops to tour test

--- test/routes/tours.test.js
+++ test/routes/tours.test.js
@@ -1,13 +1,10 @@
 require('../data-helper');
 const request = require('supertest');
 const app = require('../../lib/app');
-const mongoose = require('mongoose');
+const Tour = require('../../lib/Models/Tour');
 
 describe('tour routes', () => {
-  const id = new mongoose.Types.ObjectId;
-  const date = new Date();
-//   const testDate = date.toDateString();
-
+  //   const testDate = date.toDateString();
   const stop = {
     location: {
       lat: 44.44,
@@ -38,23 +35,67 @@ describe('tour routes', () => {
       });
   });
 
-  it.only('makes a new tour', () => {
-    // const date = new Date();
+  it('gets all tours', () => {
+    return Tour
+      .create(tour)
+      .then(() => {
+        return request(app)
+          .get('/tours')
+          .then(tour => {
+            expect(tour.body).toHaveLength(1);
+          });
+      });
+  });
+
+  it('gets a tour by id', () => {
+    return Tour
+      .create(tour)
+      .then(created => {
+        return request(app)
+          .get(`/tours/${created._id}`)
+          .then(tour => {
+            expect(tour.body).toEqual({
+              title: 'stuff i want to eat',
+              activities: ['eating', 'walking', 'more eating'],
+              stops: [],
+              launchDate: expect.any(String),
+              _id: expect.any(String),
+            });
+          });
+      });
+  });
+
+  it.only('adds a stop to a tour', () => {
     return request(app)
       .post('/tours')
       .send(tour)
-      .then(tour => {
-        expect(tour.body).toEqual({
+      .then(res => {
+        return request(app)
+          .post(`/tours/${res.body._id}/stops`)
+          .send(stop);
+      })
+      .then(res => {
+        expect(res.body).toEqual({
           title: 'stuff i want to eat',
           activities: ['eating', 'walking', 'more eating'],
-          stops: [],
           launchDate: expect.any(String),
           _id: expect.any(String),
-          __v: 0
+          stops: [{
+            location: {
+              lat: 44.44,
+              lon: 123.45
+            },
+            attendance: 2,
+            weather: {
+              temp_max: expect.any(Number),
+              temp_min: expect.any(Number),
+              description: expect.any(String)
+            }
+          }],
         });
       });
   });
 
-
 });
+
 
